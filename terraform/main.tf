e1509b8845313c20698e760ae6913f4203acc5f3
Sahasra <sahasra.cloud@gmail.com>
2024-06-16 10:04:47 +0530

    s3 added

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
 terraform {
 
   required_version = "1.8.5"
-  # backend "s3" {
-  #   bucket = "sahasra-terraform-state" # Replace with your bucket name
-  #   key    = "terraform.tfstate"       # This can be any path within the bucket
-  #   region = "ap-south-1"              # Replace with your bucket region
-  # }
+  backend "s3" {
+    bucket = "sahasra-terraform-state" # Replace with your bucket name
+    key    = "terraform.tfstate"       # This can be any path within the bucket
+    region = "ap-south-1"              # Replace with your bucket region
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -17,6 +17,5 @@ terraform {
 
 provider "aws" {
   # Configuration options
-  region = "ap-south-1" # Specify your AWS region
-  
+  region = "ap-south-1" # Specify your AWS region  
 }
